0e011fac542b1b8a62e0c4b9fd8a5924ce6a2e7b
joaovv-vitor <jrodriguescontatato@gmail.com>
2026-08-05 15:35:34 -0300

    feat: configure HTTP server address and timeouts

--- cmd/phraseforge-api/main.go
+++ cmd/phraseforge-api/main.go
@@ -3,21 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/joaovv-Vitor/phraseforge/internal/httpapi"
 	"github.com/joaovv-Vitor/phraseforge/internal/storage"
 )
 
 func main() {
-	const address = ":8080"
-
 	categories, err := storage.LoadCategories("data/phrases.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("PhraseForge API listening on %s", address)
-	if err := http.ListenAndServe(address, httpapi.NewHandler(categories)); err != nil {
+	server := &http.Server{
+		Addr:              apiAddress(),
+		Handler:           httpapi.NewHandler(categories),
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       60 * time.Second,
+	}
+
+	log.Printf("PhraseForge API listening on %s", server.Addr)
+	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
 }
